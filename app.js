7e1fa6aed402d49c985693a3faee92a7c6674f9b
Adeyemi Akinyemi <adeyemiakinyemi01@gmail.com>
2023-01-04 11:00:07 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,24 +5,29 @@ const app = Vue.createApp({
       lastName: "Doe",
       email: "jon@gmail.com",
       gender: "male",
-      picture: "https://randomuser.me/api/portraits/men/69.jpg"
+      picture: "https://randomuser.me/api/portraits/men/69.jpg",
     };
   },
   filters: {
     uppercase: function (value) {
       if (!value) return "";
       return value.toUpperCase();
-    }
+    },
   },
   methods: {
-    getUser() {
-      this.firstName = "Jane";
-      this.lastName = "Doe";
-      this.email = "jane@gmail.com";
-      this.gender = "female";
-      this.picture = "https://randomuser.me/api/portraits/women/9.jpg";
-    }
-  }
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { result } = await res.json();
+
+      // console.log(result);
+
+      this.firstName = result[0].name.first;
+      this.lastName = result[0].name.last;
+      this.email = result[0].email;
+      this.gender = result[0].gender;
+      this.picture = result[0].picture.large;
+    },
+  },
 });
 
 app.mount("#app");
